c7ab4d065650b832a76f7b6e1d78bbf2f48e9636
kasmide <email@ksmd.dev>
2024-11-13 14:01:42 +0900

    use home-manager 24.05

--- home/flake.nix
+++ home/flake.nix
@@ -5,7 +5,7 @@
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
