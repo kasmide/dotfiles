1e1764259390cdaa7f640d006d5fc7d4f7ad5ed8
kasmide <email@ksmd.dev>
2025-04-04 12:18:09 +0900

    Add Rclone serve

--- nixos/mini/configuration.nix
+++ nixos/mini/configuration.nix
@@ -117,6 +117,7 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tomhi = {
+    linger = true;
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     shell = pkgs.zsh;
