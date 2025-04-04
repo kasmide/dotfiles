d84eb091636e0849fba0608c2014640153381d74
kasmide <email@ksmd.dev>
2025-04-04 11:14:19 +0900

    Forgejo: Update version

--- nixos/mini/arion-gitea.nix
+++ nixos/mini/arion-gitea.nix
@@ -3,7 +3,7 @@
   project.name = "gitea";
   services = {
     "gitea".service = {
-      image = "codeberg.org/forgejo/forgejo:9";
+      image = "codeberg.org/forgejo/forgejo:10";
       restart = "always";
       ports = [ "8082:3000" "2222:22" ];
       environment = {
