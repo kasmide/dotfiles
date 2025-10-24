c5fe27fa6724a596dbd3662d5fed0f4b7f7d8522
kasmide <email@ksmd.dev>
2025-10-24 14:21:01 +0900

    home-manager: populate vscode settings

--- home/modules/vscode/flake.nix
+++ home/modules/vscode/flake.nix
@@ -6,8 +6,15 @@
   };
 
   outputs = { nix-vscode-extensions, ... }: {
-    homeManagerModules.default = { pkgs, ... }: {
+    homeManagerModules.default = { pkgs, lib, ... }: {
       nixpkgs.overlays = [ nix-vscode-extensions.overlays.default ];
+
+      nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+        "vscode-extension-anthropic-claude-code"
+        "vscode-extension-github-copilot"
+        "vscode-extension-github-copilot-chat"
+        "vscode-extension-ms-python-vscode-pylance"
+      ];
 
       programs.vscode = {
         enable = true;
@@ -17,12 +24,29 @@
           enableUpdateCheck = false;
           enableExtensionUpdateCheck = false;
 
-          userSettings = { };
+          userSettings = {
+            "github.copilot.nextEditSuggestions.enabled" = true;
+            "workbench.colorTheme" = "Quiet Light";
+            "editor.formatOnPaste" = true;
+            "editor.autoIndentOnPaste" = true;
+            "editor.bracketPairColorization.independentColorPoolPerBracketType" = true;
+            "editor.cursorSmoothCaretAnimation" = "on";
+          };
 
           extensions = with pkgs.nix-vscode-extensions.vscode-marketplace; [
-          ] ++ (with pkgs.nix-vscode-extensions.open-vsx; [
-            continue.continue
+            anthropic.claude-code
             asciidoctor.asciidoctor-vscode
+            bbenoist.nix
+            github.copilot
+            github.copilot-chat
+            jakebecker.elixir-ls
+            ms-python.debugpy
+            ms-python.python
+            ms-python.vscode-pylance
+            ms-python.vscode-python-envs
+            rust-lang.rust-analyzer
+            oderwat.indent-rainbow
+          ] ++ (with pkgs.nix-vscode-extensions.open-vsx; [
           ]);
         };
       };
